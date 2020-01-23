656023cb2de40dcb72a09cc97714d0dcb60d1032
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-01-23 14:17:17 +0100

    Add algorithms and evaluate subpackages. \n Algorithms: EASE \n EvaluationMethods: FoldInPercentage \n Metrics: Recall@K, NDCG@K

---
+++ recpack/evaluate/evaluate.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+from itertools import groupby
+
+import numpy
+import scipy.sparse
+
+
+class Evaluator:
+    pass
+
+
+class LeavePOutCrossValidation(Evaluator):
+
+    def __init__(self, p, sp_mat=None, shape=None):
+        self.p = p
+        self.sp_mat = sp_mat
+
+
+class FoldInPercentage(Evaluator):
+    class FoldIterator:
+        def __init__(self, fold_in_percentage):
+            self._fip = fold_in_percentage
+            self._index = 0
+            self._max_index = fold_in_percentage.sp_mat_tr.shape[0]
+
+        def __next__(self):
+            if self._index < self._max_index:
+                fold_in = self._fip.sp_mat_tr[self._index]
+                fold_out = self._fip.sp_mat_te[self._index]
+
+                self._index += 1
+
+                return fold_in, fold_out
+            raise StopIteration
+
+    def __init__(self, fold_in, sp_mat=None, shape=None, seed=None):
+        self.fold_in = fold_in
+
+        self.sp_mat_tr = None
+        self.sp_mat_te = None
+
+        if sp_mat:
+            sp_mat_tr, sp_mat_te = self.split(sp_mat, shape=shape)
+
+        self.seed = seed if seed else 12345
+
+    def split(self, sp_mat, shape=None):
+        """
+        Have to set shape specifically else it might turn out too small.
+        """
+        if not shape:
+            shape = sp_mat.shape
+
+        U, I, V = scipy.sparse.find(sp_mat)
+
+        items_by_user_dct = defaultdict(list)
+
+        # Unsure if U and I are sorted, which is a requirement for itertools.groupby. So add them to a defaultdict to be sure
+        groups = groupby(zip(U, I, V), lambda x: x[0])
+        for key, subiter in groups:
+            items_by_user_dct[key].extend(subiter)
+
+        in_fold, out_fold = [], []
+
+        for u in items_by_user_dct.keys():
+            usr_hist = items_by_user_dct[u]
+
+            _len = len(usr_hist)
+
+            numpy.random.shuffle(usr_hist)
+
+            cut = int(numpy.floor(_len * self.fold_in))
+
+            in_fold.extend(usr_hist[:cut])
+            out_fold.extend(usr_hist[cut:])
+
+        U_tr, I_tr, V_tr = zip(*in_fold)
+
+        sp_mat_tr = scipy.sparse.csr_matrix((V_tr, (U_tr, I_tr)), shape=shape)
+
+        self.sp_mat_tr = sp_mat_tr
+
+        U_te, I_te, V_te = zip(*out_fold)
+        sp_mat_te = scipy.sparse.csr_matrix((V_te, (U_te, I_te)), shape=shape)
+
+        self.sp_mat_te = sp_mat_te
+
+        return sp_mat_tr, sp_mat_te
+
+    def __iter__(self):
+        return self.FoldIterator(self)
---
+++ recpack/evaluate/metrics.py
@@ -0,0 +1,69 @@
+import numpy
+
+
+class Metric:
+    pass
+
+
+class RecallK(Metric):
+
+    def __init__(self, K):
+        self.K = K
+        self.recall = 0
+        self.num_users = 0
+
+    def update(self, X_pred, X_true):
+
+        topK_list = numpy.argpartition(X_pred, -self.K)[-self.K:]
+        topK_set = set(topK_list)
+
+        items = X_true.indices
+
+        self.recall += len(topK_set.intersection(items)) / min(self.K, len(items))
+        self.num_users += 1
+
+        return
+
+    @property
+    def value(self):
+        return self.recall / self.num_users
+
+
+class NDCGK(Metric):
+
+    def __init__(self, K):
+        self.K = K
+        self.NDCG = 0
+        self.num_users = 0
+
+        self.discount_template = 1. / numpy.log2(numpy.arange(2, K + 2))
+
+        self.IDCG = {K: self.discount_template[:K].sum()}
+
+    def update(self, X_pred, X_true):
+        topK_list = numpy.argpartition(X_pred, -self.K)[-self.K:]
+        # Extract top-K highest scores into a sorted list
+        sorted_topK_list = topK_list[numpy.argsort(X_pred[topK_list])][::-1]
+
+        items = X_true.indices
+
+        M = len(items)
+
+        if M < self.K:
+            IDCG = self.IDCG.get(M)
+
+            if not IDCG:
+                IDCG = self.discount_template[:M].sum()
+                self.IDCG[M] = IDCG
+
+        # Compute DCG
+        DCG = sum((self.discount_template[rank] * (item in items)) for rank, item in enumerate(sorted_topK_list))
+
+        self.num_users += 1
+        self.NDCG += DCG / IDCG
+
+        return
+
+    @property
+    def value(self):
+        return self.NDCG / self.num_users
