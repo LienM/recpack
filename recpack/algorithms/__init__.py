a4a4066270e841cb06abf0047bec61375cf8d984
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-02-04 14:18:07 +0100

    Move imports to __init__.py

--- recpack/algorithms/__init__.py
+++ recpack/algorithms/__init__.py
@@ -1,1 +1,13 @@
-from .algorithms import get_algorithm
+from .linear_algorithms import EASE
+from .true_baseline_algorithms import Popularity, Random
+
+
+ALGORITHMS = {
+    'ease': EASE,
+    'random': Random,
+    'popularity': Popularity
+}
+
+
+def get_algorithm(algorithm_name):
+    return ALGORITHMS[algorithm_name]
