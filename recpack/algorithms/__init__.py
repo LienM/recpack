6664bc4f22aa2dcc4705d4dc7724a04001e36718
verachtertr <verachtertr@users.noreply.github.com>
2020-02-05 15:06:44 +0100

    Added SLIM implementation to linear algorithms (#4)
    * Added SLIM implementation to linear algorithms

--- recpack/algorithms/__init__.py
+++ recpack/algorithms/__init__.py
@@ -1,4 +1,4 @@
-from .linear_algorithms import EASE
+from .linear_algorithms import EASE, SLIM
 from .true_baseline_algorithms import Popularity, Random
 from .nn_algorithms import ItemKNN
 
@@ -7,7 +7,8 @@ ALGORITHMS = {
     'ease': EASE,
     'random': Random,
     'popularity': Popularity,
-    'itemKNN': ItemKNN
+    'itemKNN': ItemKNN,
+    'slim': SLIM
 }
 
 
--- recpack/algorithms/linear_algorithms.py
+++ recpack/algorithms/linear_algorithms.py
@@ -2,6 +2,10 @@ import secrets
 
 import numpy as np
 
+import scipy.sparse
+
+from sklearn.linear_model import SGDRegressor, ElasticNet
+
 from .algorithm_base import Algorithm
 
 
@@ -62,3 +66,90 @@ class EASE(Algorithm):
         if self.B is None:
             raise Exception("Fit a model before trying to predict with it.")
         return X @ self.B
+
+
+class SLIM(Algorithm):
+    """ Implementation of the SLIM model.
+    loosely based on https://github.com/Mendeley/mrec
+    """
+    def __init__(self, l1_reg=0.0005, l2_reg=0.00005, fit_intercept=True, ignore_neg_weights=True, model='sgd'):
+
+        self.similarity_matrix = None
+
+        self.l1_reg = l1_reg
+        self.l2_reg = l2_reg
+        # Translate regression parameters into the expected sgd parameters
+        self.alpha = self.l1_reg + self.l2_reg
+        self.l1_ratio = self.l1_reg/self.alpha
+        self.fit_intercept = fit_intercept
+        self.ignore_neg_weights = ignore_neg_weights
+
+        # Construct internal model
+        # ALLOWED MODELS:
+        ALLOWED_MODELS = ['sgd']
+
+        if model == 'sgd':
+            self.model = SGDRegressor(
+                penalty='elasticnet',
+                fit_intercept=fit_intercept,
+                alpha=self.alpha,
+                l1_ratio=self.l1_ratio
+            )
+
+        else:
+            raise NotImplementedError(
+                f"{model} is not yet implemented, "
+                f"please use one of {ALLOWED_MODELS}"
+            )
+
+    def _compute_similarities(self, work_matrix, item):
+        new_matrix = work_matrix.tocoo()
+        target = new_matrix.getcol(item)
+        data_indices = np.where(new_matrix.col == item)[0]
+        new_matrix.data[data_indices] = 0
+        self.model.fit(new_matrix, target.toarray().ravel())
+
+        w = self.model.coef_
+        if self.ignore_neg_weights:
+            w[w < 0] = 0
+        return w
+
+    def fit(self, X):
+        """Fit a similarity matrix based on data X.
+
+        X is an m x n binary matrix of user item interactions.
+        Where m is the number of users, and n the number of items.
+        """
+        # Prep sparse representation inputs
+        data = []
+        row = []
+        col = []
+        # Loop over all items
+        for j in range(X.shape[1]):
+            # Compute the contribution values of all other items for the item j using linear regression
+            w = self._compute_similarities(X, j)
+            # Update sparse repr. inputs.
+            # w[i,j] = the contribution of item i to predicting item j
+            for i in w.nonzero()[0]:
+                data.append(w[i])
+                row.append(i)
+                col.append(j)
+
+        # Construct similarity matrix.
+        # Shape is determined by 2nd dimension of the shape of input matrix X
+        self.similarity_matrix = scipy.sparse.csr_matrix((data, (row, col)), shape=(X.shape[1], X.shape[1]))
+
+    def predict(self, X):
+        """Predict scores for each user, item pair
+        X is a user item interaction matrix in sparse represenation size m' x n
+        Where n needs to be the same as the n in fit, but m' can be anything.
+
+        response will be a m' x n matrix again with predicted scores.
+
+        No history is actually filtered.
+        """
+        if self.similarity_matrix is None:
+            raise Exception("Fit a model before trying to predict with it.")
+        # TODO, this looks exactly the same as NN's recommendation -> refactor into a similarity based class.
+        scores = X @ self.similarity_matrix
+        return scores.toarray()
