27117231ae80cfb237f5f7cb760dbec57d426fe4
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-02-14 15:14:53 +0100

    Remove potentially sensitive files

--- recpack/env.py
+++
@@ -1,8 +0,0 @@
-
-
-class TrainingConfiguration:
-    BUCKET_NAME = 'lien-synthetic-data-experiments'
-    DATASET_NAME = 'ml25'
-    TRAINING_DATA_FILENAME = 'item_interactions_tr.csv'
-    EXPERIMENT_NAME = 'experiment_2020-01-17T13:16:41.747147+00:00'
-    ALGORITHM_NAME = 'ease'
