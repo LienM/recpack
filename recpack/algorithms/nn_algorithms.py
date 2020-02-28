b12c1728b169aff4c7b6a4443acb7bfa8449310e
Robin Verachtert <robinverachtert@MacBook-Pro-Robin-Verachtert.local>
2020-02-28 16:26:25 +0100

    Added NotItemKNN which contains our earlier faulty item knn implementation

--- recpack/algorithms/nn_algorithms.py
+++ recpack/algorithms/nn_algorithms.py
@@ -58,3 +58,52 @@ class SharedAccount(ItemKNN):
 
     def predict(self, X):
         raise NotImplementedError("Under construction, the gnomes are working on it.")
+
+
+class NotItemKNN(Algorithm):
+    """
+    TODO: Figure out what this code is actually implementing. It is not cosine similarity
+    It does seem to work fine though.
+    """
+
+    def __init__(self, K=200):
+        """Construct an ItemKNN model. Before use make sure to fit the model.
+        The K parameter defines the how much best neighbours are kept for each item."""
+        self.K = K
+        self.item_cosine_similarities = None
+
+    def fit(self, X):
+        """Fit a cosine similarity matrix from item to item"""
+        co_mat = X.T @ X
+        # Do the cosine similarity computation here, this way we can set the diagonal to zero
+        # to avoid self recommendation
+        A = diags(1 / co_mat.diagonal())
+
+        # This has all item-cosine similarities. Now we should probably set N-K to zero
+        self.item_cosine_similarities = A @ co_mat
+
+        # Set diagonal to 0, because we don't support self similarity
+        self.item_cosine_similarities.setdiag(0)
+
+        # resolve top K per item
+        # Get indices of top K items per item
+        indices = [
+            (i, j)
+            for i, best_items_row in enumerate(np.argpartition(self.item_cosine_similarities.toarray(), -self.K))
+            for j in best_items_row[-self.K:]
+        ]
+        # Create a mask matrix which will be pointwise multiplied with the similarity matrix.
+        mask = scipy.sparse.csr_matrix(([1 for i in range(len(indices))], (list(zip(*indices)))))
+        self.item_cosine_similarities = self.item_cosine_similarities.multiply(mask)
+
+    def predict(self, X):
+        # Use total sum of similarities
+        # TODO: Use average?
+        if self.item_cosine_similarities is None:
+            raise Exception("Fit a model before trying to predict with it.")
+        scores = X @ self.item_cosine_similarities
+        return scores.toarray()
+
+    @property
+    def name(self):
+        return f"item_knn_{self.K}"
