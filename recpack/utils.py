f9efc395ef822d7fe3115941e1f3bd1d49da7f87
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-04-21 11:15:59 +0200

    Avoid getting the same log statement ten times

--- recpack/utils.py
+++ recpack/utils.py
@@ -6,12 +6,11 @@ import logging
 def get_logger():
     logger = logging.getLogger("recpack")
 
-    logger.setLevel(logging.DEBUG)
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     return logger
