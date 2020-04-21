8f6f53e1733b47df5e1404b7811303651c033fdd
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-04-21 11:01:11 +0200

    Don't forget to add the actual logger, moron

---
+++ recpack/utils.py
@@ -0,0 +1,17 @@
+import sys
+import logging.config
+import logging
+
+
+def get_logger():
+    logger = logging.getLogger("recpack")
+
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
