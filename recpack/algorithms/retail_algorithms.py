75dfc4123b4408821333bce086f75509ccb8bbe7
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-04-24 14:39:03 +0200

    Add retail algorithms + Fix assert in basic metrics tests

---
+++ recpack/algorithms/retail_algorithms.py
@@ -0,0 +1,125 @@
+from typing import Union
+
+import numpy as np
+import scipy.sparse as sp
+
+from recpack.algorithms.algorithm_base import Algorithm
+
+
+class ProductLabeler:
+    def __init__(self):
+        """
+        ProductLabeler labels the purchase history with durable or consumable.
+        """
+        self.is_fit = False
+        self.user_discount = None
+
+    def fit(self, labels, purchases):
+        """
+        Fit the ProductLabeler so that if an item is both durable
+        and it occurs in the user's purchase history,
+        it is labeled 1, otherwise zero.
+        """
+        self.user_discount = purchases.multiply(labels)
+        self.is_fit = True
+
+    def get_durable(self, X):
+        """
+        Return only the part of X that is durable
+        and occured in the user's purchase history.
+        """
+        return X.multiply(self.user_discount)
+
+    def get_consumable(self, X):
+        """
+        Return only the part of X that is not both durable and occured in the user's purchase history.
+        """
+        durable = X.multiply(self.user_discount)
+
+        new_X = X.copy()
+
+        nonzero_users = list(set(X.nonzero()[0]))
+
+        new_X[nonzero_users, :] = X[nonzero_users, :] - durable[nonzero_users, :]
+
+        return new_X
+
+
+class RetailAlgorithm(Algorithm):
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        """
+        Fit a model using purchases and optionally pageviews information.
+
+        :param purchases: [description]
+        :type purchases: [type]
+        :param pageviews: [description], defaults to None
+        :type pageviews: [type], optional
+        """
+        pass
+
+    def fit_classifier(self, purchases, labels):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+class FilterDurableGoods(RetailAlgorithm):
+    def __init__(self, rec_algo, goods_classifier):
+        self.rec_algo = rec_algo
+        self.goods_classifier = goods_classifier
+
+    def fit(self, X):
+        """
+        Fit the underlying recommendation algorithm.
+        X can be pageviews or purchases data, depending on the evaluation setting.
+
+        :param X: [description]
+        :type X: [type]
+        """
+        self.rec_algo.fit(X)
+
+        if not self.goods_classifier.is_fit:
+            raise RuntimeError("Goods Classifier should have been fit")
+
+    def fit_classifier(self, purchases, labels):
+        self.goods_classifier.fit(purchases, labels)
+
+    def predict(self, X):
+        consumable_X = self.goods_classifier.get_consumable(X)
+
+        return self.rec_algo.predict(consumable_X)
+
+
+class DiscountDurableGoods(RetailAlgorithm):
+    def __init__(
+        self, rec_algo, goods_classifier, discount=1 / 3
+    ):
+        self.rec_algo = rec_algo
+        self.goods_classifier = goods_classifier
+        self.discount_value = discount
+
+    def fit(self, X):
+        """
+        Fit the underlying recommendation algorithm.
+        X can be pageviews or purchases data, depending on the evaluation setting.
+
+        :param X: [description]
+        :type X: [type]
+        """
+        if not self.goods_classifier.is_fit:
+            raise RuntimeError("Goods Classifier should have been fit up front.")
+
+        self.rec_algo.fit(X)
+
+    def predict(self, X):
+
+        consumable_X = self.goods_classifier.get_consumable(X)
+        durable_X = self.goods_classifier.get_durable(X)
+
+        return self.rec_algo.predict(
+            consumable_X
+        ) - self.discount_value * self.rec_algo.predict(durable_X)
---
+++ recpack/tests/test_algorithms/test_retail_algorithms.py
@@ -0,0 +1,80 @@
+import numpy
+
+import recpack.algorithms.retail_algorithms as retail_algorithms
+import recpack.algorithms.true_baseline_algorithms as true_baseline_algorithms
+
+
+def test_product_labeler_durable(pageviews, purchases, labels):
+    """
+    User 0 has purchased item 0,
+    User 1 has purched item 4.
+    Item 0 is durable, item 4 is not.
+    """
+    pl = retail_algorithms.ProductLabeler()
+    pl.fit(labels, purchases)
+
+    durables = pl.get_durable(pageviews)
+
+    assert durables.shape == pageviews.shape
+    assert len(durables.nonzero()[1]) == 1
+
+    assert durables.nonzero()[1][0] == 0
+
+
+def test_product_labeler_consumable(pageviews, purchases, labels):
+    """
+    User 0 has purchased item 0,
+    User 1 has purched item 4.
+    Item 0 is durable, item 4 is not.
+    """
+
+    pl = retail_algorithms.ProductLabeler()
+    pl.fit(labels, purchases)
+
+    consumables = pl.get_consumable(pageviews)
+
+    assert consumables.shape == pageviews.shape
+    assert len(consumables.nonzero()[1]) == 5
+
+    assert consumables[0, 2] == 2
+
+
+def test_filter_durable_goods(pageviews, purchases, labels):
+    pl = retail_algorithms.ProductLabeler()
+    pl.fit(labels, purchases)
+
+    base_algo = true_baseline_algorithms.Popularity(1)
+
+    true_algo = retail_algorithms.FilterDurableGoods(base_algo, pl)
+
+    true_algo.fit(pageviews)
+
+    res = true_algo.predict(purchases)
+
+    # No recommendations because purchase was filtered
+    assert len(res[0].nonzero()[1]) == 0
+
+    # Recommend item 3 (most viewed)
+    assert res[2, 3] == 1
+    assert len(res[2].nonzero()[1]) == 1
+
+
+def test_discount_durable_goods(pageviews, purchases, labels):
+    pl = retail_algorithms.ProductLabeler()
+    pl.fit(labels, purchases)
+
+    base_algo = true_baseline_algorithms.Popularity(1)
+
+    true_algo = retail_algorithms.DiscountDurableGoods(base_algo, pl, discount=0.5)
+
+    true_algo.fit(pageviews)
+
+    res = true_algo.predict(purchases)
+
+    # Purchase was discounted  so -1
+    assert len(res[0].nonzero()[1]) == 1
+    numpy.testing.assert_almost_equal(res[0, 3], -0.5)
+
+    # Recommend item 3 (most viewed)
+    assert res[2, 3] != 0
+    assert len(res[2].nonzero()[1]) == 1
