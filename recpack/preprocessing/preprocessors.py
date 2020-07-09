fix copy paste error and change default value for normalization to False

recpack/preprocessing/preprocessors.py
@@ -56,9 +56,9 @@ class DataFramePreprocessor:
                 "User ID Mapping should be fit before attempting to map users"
             )
 
-        user_id_mapping_array = np.empty(max(self.user_id_mapping.keys()) + 1)
+        user_id_mapping_array = np.arange(0, max(self.user_id_mapping.keys()) + 1)
         user_id_mapping_array[list(self.user_id_mapping.keys())] = list(self.user_id_mapping.values())
-        res = user_id_mapping_array[df[self.item_id]]
+        res = user_id_mapping_array[df[self.user_id]]
         logger.debug("Done")
         return res
         # return df[self.user_id].progress_map(lambda x: self.user_id_mapping.get(x))
@@ -70,7 +70,7 @@ class DataFramePreprocessor:
                 "Item ID Mapping should be fit before attempting to map items"
             )
 
-        item_id_mapping_array = np.empty(max(self.item_id_mapping.keys()) + 1)
+        item_id_mapping_array = np.arange(0, max(self.item_id_mapping.keys()) + 1)
         item_id_mapping_array[list(self.item_id_mapping.keys())] = list(self.item_id_mapping.values())
         res = item_id_mapping_array[df[self.item_id]]
         logger.debug("Done")