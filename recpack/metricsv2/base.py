f3fca2594a0442198c7deb7b73eafb604d80b8a5
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-08-03 09:54:45 +0200

    Some changes to the Base metric class

--- recpack/metricsv2/base.py
+++ recpack/metricsv2/base.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 import pandas as pd
+
 from sklearn.base import BaseEstimator
 
 import logging
@@ -9,13 +10,28 @@ from recpack.data.data_matrix import DataM
 
 logger = logging.getLogger("recpack")
 
+
 class Metric:
+
+    def __init__(self):
+        super().__init__()
+        self._num_users = 0
+        self._num_items = 0
+        self._value = 0
 
     @property
     def name(self):
         return str(self.__class__).lower()
 
     def calculate(self, y_true: csr_matrix, y_pred: csr_matrix) -> None:
+        """
+        Calculates this Metric for all users.
+
+        :param y_true: True user-item interactions.
+        :type y_true: csr_matrix
+        :param y_pred: Predicted affinity of users for items.
+        :type y_pred: csr_matrix
+        """
         pass
 
     @property
@@ -24,7 +40,26 @@ class Metric:
 
     @property
     def value(self) -> float:
-        pass
+        return self._value
+
+    @property
+    def num_items(self) -> int:
+        return self._num_items
+
+    @property
+    def num_users(self) -> int:
+        return self._num_users
+
+    def verify_shape(self, y_true: csr_matrix, y_pred: csr_matrix) -> bool:
+        check = y_true.shape == y_pred.shape
+
+        if not check:
+            raise AssertionError(f"Shape mismatch between y_true: {y_true.shape} and y_pred: {y_pred.shape}")
+        else:
+            # TODO Maybe this should be a separate method?
+            self._num_users, self._num_items = y_true.shape
+
+        return True
 
 
 class ElementwiseMetric(Metric):
@@ -61,6 +96,7 @@ class MetricTopK(Metric):
     """
     Base class for any metric computed on the TopK items for a user.
     """
+    #TODO Verify if this should be a Mixin
     def __init__(self, K):
         super().__init__()
         self.K = K
@@ -74,7 +110,7 @@ class MetricTopK(Metric):
         nonzero_users = sorted(list(set(y_pred.nonzero()[0])))
         X = y_pred[nonzero_users, :].toarray()
 
-        items = np.argpartition(X, -self.K)[:, -self.K:]
+        items = np.argpartition(X, -self.K)[:, -self.K :]
 
         U, I, V = [], [], []
 
@@ -83,16 +119,16 @@ class MetricTopK(Metric):
             I.extend(items[ix])
             V.extend(X[ix, items[ix]])
 
-        y_pred_top_K = csr_matrix(
-            (V, (U, I)), dtype=y_pred.dtype, shape=y_pred.shape
-        )
+        y_pred_top_K = csr_matrix((V, (U, I)), dtype=y_pred.dtype, shape=y_pred.shape)
 
         return y_pred_top_K
+
 
 class FittedMetric(Metric, BaseEstimator):
     """
     Base class for all metrics that need to be fit on a training set
     before they can be used.
     """
+
     def fit(self, X: csr_matrix):
         pass
