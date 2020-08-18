e63b7b0b3fbe03db3b08331f4da987ab005222b9
Joey De Pauw <joeydepauw@gmail.com>
2020-08-18 12:52:26 +0200

    added test for calibrated recall and symmetric case for recall

--- recpack/tests/test_metrics/test_recall.py
+++ recpack/tests/test_metrics/test_recall.py
@@ -1,6 +1,6 @@
 import numpy
 
-from recpack.metrics.recall import RecallK
+from recpack.metrics.recall import RecallK, CalibratedRecallK
 
 
 def test_recallK(X_pred, X_true):
@@ -10,3 +10,26 @@ def test_recallK(X_pred, X_true):
     metric.calculate(X_true, X_pred)
 
     numpy.testing.assert_almost_equal(metric.value, 2 / 3)
+
+    K = 1
+    metric = RecallK(K)
+
+    metric.calculate(X_true, X_pred)
+
+    numpy.testing.assert_almost_equal(metric.value, 0.25)
+
+
+def test_calibrated_recallK(X_pred, X_true):
+    K = 2
+    metric = CalibratedRecallK(K)
+
+    metric.calculate(X_true, X_pred)
+
+    numpy.testing.assert_almost_equal(metric.value, 0.75)
+
+    K = 1
+    metric = CalibratedRecallK(K)
+
+    metric.calculate(X_true, X_pred)
+
+    numpy.testing.assert_almost_equal(metric.value, 0.5)
