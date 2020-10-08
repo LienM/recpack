75d2251db6410b2412893ae8ca858e7e46a28978
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-10-08 17:43:27 +0200

    Add WARP Sampling + Test WARP Sampling

--- recpack/algorithms/metric_learning/CML.py
+++ recpack/algorithms/metric_learning/CML.py
@@ -1,3 +1,7 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+import torch
 from recpack.algorithms.base import Algorithm
 
 
@@ -41,14 +45,52 @@ class CMLWithFeatures(Algorithm):
         use_cov_loss,
         hidden_layer_dim,
         feature_l2_reg,
-        feature_proj_scaling_factor
+        feature_proj_scaling_factor,
     ):
         pass
 
 
-class RandomNegativeSampler:
+# TODO Integrate sampling methods somewhere more logical
+def warp_sample_pairs(X: csr_matrix, U=10, batch_size=100) -> torch.LongTensor:
     """
-    Sample random negative samples for this user uniformly. 
-    """
+    Sample U negatives for every user-item-pair (positive).
 
-    pass
+    :param X: Interaction matrix
+    :type X: csr_matrix
+    :param batch_size: The number of samples returned per batch, defaults to 100
+    :type batch_size: int, optional
+    :yield: tensor of shape (batch_size, U+1), with user, positive item, U negative items for each row.
+    :rtype: torch.LongTensor
+    """
+    # Need positive and negative pair. Requires the existence of a positive for this item.
+    positives = np.array(X.nonzero()).T  # As a (num_interactions, 2) numpy array
+    num_positives = positives.shape[0]
+    np.random.shuffle(positives)
+
+    # TODO Could be better to only yield this when required, to keep the memory footprint low.
+
+    for start in range(0, num_positives, batch_size):
+        positives_batch = positives[start: start + batch_size]
+
+        # Important only for final batch, if smaller than batch_size
+        true_batch_size = min(batch_size, num_positives - start)
+
+        negatives_batch = np.random.randint(0, X.shape[1], size=(true_batch_size, U))
+        while True:
+            # Fix the negatives that are equal to the positives, if there are any
+            mask = np.apply_along_axis(lambda col: col == positives_batch[:, 1], 0, negatives_batch)
+            num_incorrect = np.sum(mask)
+
+            if num_incorrect > 0:
+                new_negatives = np.random.randint(
+                    0, X.shape[1], size=(num_incorrect,)
+                )
+                negatives_batch[mask] = new_negatives
+            else:
+                # Exit the while loop
+                break
+
+        sample_pairs_batch = np.empty((positives_batch.shape[0], U + 2))
+        sample_pairs_batch[:, :2] = positives_batch
+        sample_pairs_batch[:, 2:] = negatives_batch
+        yield torch.LongTensor(sample_pairs_batch)
