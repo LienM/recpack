fd016c9e40cdd65d607e61481e045e4e527d1d2d
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2020-10-15 12:02:02 +0200

    Fix samplers test with new interface

--- recpack/tests/test_algorithms/test_samplers.py
+++ recpack/tests/test_algorithms/test_samplers.py
@@ -8,14 +8,14 @@ def test_warp_sampling(pageviews):
     batch_size = 4
     U = 10
 
-    res = warp_sample_pairs(pageviews, U=U, batch_size=batch_size)
-
     total_interactions = 0
 
-    for batch in res:
-        b = batch.shape[0]
+    for users, pos_interactions, neg_interactions in warp_sample_pairs(pageviews, U=U, batch_size=batch_size):
+        b = users.shape[0]
         assert (b == batch_size) or (b == pageviews.nnz % batch_size)
-        assert batch.shape[1] == U + 2
-        total_interactions += batch.shape[0]
+        assert users.shape[0] == pos_interactions.shape[0]
+        assert users.shape[0] == neg_interactions.shape[0]
+        assert neg_interactions.shape[1] == U
+        total_interactions += users.shape[0]
 
     assert total_interactions == pageviews.nnz
