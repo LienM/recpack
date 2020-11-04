e38984f5a7f289c8c3c21c3e8ac3fcb0a4091154
Olivier Bellemans <olivier.bellemans@student.uantwerpen.be>
2020-11-04 10:25:03 +0100

    Convert to same type, convert tuple

--- recpack/data/matrix.py
+++ recpack/data/matrix.py
@@ -5,29 +5,22 @@ import pandas as pd
 
 from recpack.data.data_matrix import DataM, USER_IX, ITEM_IX, VALUE_IX, TIMESTAMP_IX
 from scipy.sparse import csr_matrix
-from typing import Union, Any, Optional
+from typing import Union, Any, Optional, Tuple
 
 
 Matrix = Union[DataM, csr_matrix]
 _supported_types = Matrix.__args__  # type: ignore
 
 
-def is_supported(t: Any) -> bool:
-    """
-    Returns whether a given matrix type is supported by recpack.
-    """
-    if not isinstance(t, type):
-        t = type(t)
-    return issubclass(t, _supported_types)
-
-
-def to_csr_matrix(X: Matrix) -> csr_matrix:
+def to_csr_matrix(X: Union[Matrix, Tuple[Matrix, ...]]) -> csr_matrix:
     """
     Converts any matrix to a scipy csr_matrix.
 
-    :param X: Matrix-like object to convert
+    :param X: Matrix-like object or tuple of objects to convert
     :raises: UnsupportedTypeError
     """
+    if isinstance(X, (tuple, list)):
+        return type(X)(to_csr_matrix(x) for x in X)
     if isinstance(X, csr_matrix):
         return X
     if isinstance(X, DataM):
@@ -35,15 +28,17 @@ def to_csr_matrix(X: Matrix) -> csr_matrix:
     raise UnsupportedTypeError(X)
 
 
-def to_datam(X: Matrix, require_timestamps: bool = False) -> DataM:
+def to_datam(X: Union[Matrix, Tuple[Matrix, ...]], timestamps: bool = False) -> DataM:
     """
     Converts any matrix to recpack's DataM.
 
-    :param X: Matrix-like object to convert
-    :param require_timestamps: Fail if timestamp information is unavailable
+    :param X: Matrix-like object or tuple of objects to convert
+    :param timestamps: Require timestamp information, fail if none is available
     :raises: UnsupportedTypeError, InvalidConversionError
     """
-    if require_timestamps and not _get_timestamps(X):
+    if isinstance(X, (tuple, list)):
+        return type(X)(to_datam(x, timestamps) for x in X)
+    if timestamps and not _get_timestamps(X):
         raise InvalidConversionError("Source matrix has no timestamp information.")
     if isinstance(X, csr_matrix):
         df = _matrix_to_df(X)
@@ -51,6 +46,20 @@ def to_datam(X: Matrix, require_timestamps: bool = False) -> DataM:
     if isinstance(X, DataM):
         return X
     raise UnsupportedTypeError(X)
+
+
+def to_same_type(X: Matrix, Y: Matrix) -> Matrix:
+    """
+    Converts any matrix to the same type as a target matrix.
+
+    :param X: Matrix-like object to convert
+    :param Y: Matrix-like object with the desired type
+    :raises: UnsupportedTypeError, InvalidConversionError
+    """
+    f_conv = {csr_matrix: to_csr_matrix, DataM: to_datam}.get(type(Y))
+    if not f_conv:
+        raise UnsupportedTypeError(Y)
+    return f_conv(X)
 
 
 def _matrix_to_df(X: csr_matrix) -> pd.DataFrame:
@@ -66,9 +75,18 @@ def _get_timestamps(X: Matrix) -> Optional[pd.Series]:
     Returns timestamp information as a pandas Series if available, None if not.
     """
     try:
-        return X.timestamps()
+        return X.timestamps
     except:
         return None
+
+
+def _is_supported(t: Any) -> bool:
+    """
+    Returns whether a given matrix type is supported by recpack.
+    """
+    if not isinstance(t, type):
+        t = type(t)
+    return issubclass(t, _supported_types)
 
 
 class UnsupportedTypeError(Exception):
@@ -79,7 +97,7 @@ class UnsupportedTypeError(Exception):
     """
 
     def __init__(self, X: Any):
-        assert not is_supported(X)
+        assert not _is_supported(X)
         super().__init__(
             "Recpack only supports matrix types {}. Received {}.".format(
                 ", ".join(t.__name__ for t in _supported_types), type(X).__name__
