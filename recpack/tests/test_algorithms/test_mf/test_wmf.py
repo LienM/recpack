cb1782f0f6ed448271ec7998b458641705a3786b
joostatsooj <joostatsooj@gmail.com>
2020-11-10 13:41:51 +0100

    unit test for linear equation

--- recpack/tests/test_algorithms/test_mf/test_wmf.py
+++ recpack/tests/test_algorithms/test_mf/test_wmf.py
@@ -4,7 +4,7 @@ from recpack.algorithms import WeightedMatrixFactorization
 
 
 def test_wmf():
-    wmf = WeightedMatrixFactorization(cs='log-scaling', num_components=10, iterations=10000)
+    wmf = WeightedMatrixFactorization(cs='log-scaling', num_components=10, iterations=5000)
 
     values = [2, 5, 4, 1, 3, 4, 3]
     users = [0, 0, 1, 1, 2, 2, 2]
@@ -30,3 +30,18 @@ def test_wmf():
     exp_items = [0, 1, 2, 0, 1, 2]
     expected_prediction = csr_matrix((exp_values, (exp_users, exp_items)), shape=test_matrix.shape)
     numpy.testing.assert_almost_equal(prediction.toarray(), expected_prediction.toarray(), decimal=2)
+
+
+def test_linear_equation():
+    Y = numpy.array([[1, 0], [5, 5], [0, 1]])
+    YtY = Y.T.dot(Y)
+
+    values = [2, 5, 4, 1, 3, 4, 3]
+    users = [0, 0, 1, 1, 2, 2, 2]
+    items = [1, 2, 0, 2, 0, 1, 2]
+    test_matrix = csr_matrix((values, (users, items)))
+
+    wmf = WeightedMatrixFactorization(num_components=2, regularization=0.1)
+    result = wmf._linear_equation_3(Y, YtY, test_matrix, 0)
+
+    numpy.testing.assert_almost_equal(result, numpy.array([-0.66, 0.86]), decimal=2)
