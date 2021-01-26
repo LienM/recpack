7d2106c373b0c396fdcb46b1e5ad04158dd3386d
joostatsooj <joostatsooj@gmail.com>
2021-01-26 19:42:28 +0100

    right implementation for AUC AMAN

--- recpack/metrics/auc_aman.py
+++ recpack/metrics/auc_aman.py
@@ -5,51 +5,66 @@ import scipy.sparse
 from recpack.util import get_top_K_ranks
 from scipy.sparse import csr_matrix
 
-from recpack.metrics.base import ElementwiseMetricK
-
+from recpack.metrics.base import ListwiseMetricK
 
 logger = logging.getLogger("recpack")
 
-
-class AUCAMAN(ElementwiseMetricK):
+class AUCAMAN(ListwiseMetricK):
     """
     Measure is described in paper of S. Rendle, C. Freudenthaler, Z. Gantner, and L. Schmidt-Thieme. Bpr: Bayesian
     personalized ranking from implicit feedback. In UAI, pages 452–461, 2009
-    It is an AUC measure where a missing preference is evaluated as a dislike; given by the following formula:
-        AUC_AMAN = 1 / |U_t| \\sum_{u \\in U_t}{\\frac{|I| - rank(h_u)}{|I| - 1}}
-        where H_u is the set containing all test preferences of that user
+    The measure is implemented as worked out in equation 2.
     """
 
     def __init__(self):
         super().__init__(None)
 
-    def calculate(self, y_true: csr_matrix, y_pred: csr_matrix) -> None:
+    def calculate(self, y_true: csr_matrix, y_pred: csr_matrix, X: csr_matrix) -> None:
         """
         Calculate the AUC AMAN score for the particular y_true and y_pred matrices.
-        :param y_true: User-item matrix with the actual true rating values.
+        :param y_true: User-item matrix with the actual true binary rating values.
         :param y_pred: User-item matrix with all prediction rating scores.
+        :param X: User-item matrix with all binary rating interactions.
         :return: None: The result will be saved in self.value.
         """
-        y_true, y_pred = self.eliminate_empty_users(y_true, y_pred)
         self.verify_shape(y_true, y_pred)
 
-        _, items = y_true.shape
-        assert items > 1
+        num_users, num_items = X.shape
 
-        y_pred_top_K = get_top_K_ranks(y_pred)
+        y_pred_top_K = get_top_K_ranks(y_pred, self.K)
         self.y_pred_top_K_ = y_pred_top_K
-        scores = scipy.sparse.lil_matrix(y_pred.shape)
+
+        # Rank of the matrix I
+        ranking = scipy.sparse.lil_matrix(X.shape)
 
         # Elementwise multiplication of top K predicts and true interactions
-        scores[y_pred_top_K.multiply(y_true).astype(np.bool)] = 1
-        # Element-wise multiplication. Result is a matrix with the rank of the predictions and the
-        scores = scores.multiply(y_pred_top_K)
-        scores.data = (items - scores.data) / (items - 1)
+        ranking[y_pred_top_K.multiply(y_true).astype(np.bool)] = 1
+        ranking = ranking.multiply(y_pred_top_K)
+        ranking = ranking.tocsr()  # contains the ranks for the i elements in E(u)
 
-        scores = scores.tocsr()
+        J = X + y_true  # X and y_true: not in J
+
+        scores = csr_matrix((num_users, 1))
+
+        for u in range(num_users):
+            counter = 1
+            rank_u = ranking[u, :]
+            if rank_u.nnz == 0:  # is empty
+                continue
+            I_rank = sorted(rank_u.data)
+
+            J_u = J[u, :]
+            E_u = num_items - len(J_u.data)
+            sum = 0
+
+            # For all i:
+            #  SUM (E(u) - rank_i + counter) / E(u)
+            for rank in I_rank:
+                sum += (E_u - rank + counter) / E_u
+                counter += 1
+
+            scores[u, 0] = sum / len(I_rank)  # divide by length of I_rank to scale between 0 and 1
+
         self.scores_ = scores
-
-        # Value is the mean of the scores. 1 / |U_t| * sum_(u in U_t) scores
-        self.value_ = scores.data.mean()
 
         return
--- recpack/tests/test_metrics/test_auc_aman.py
+++ recpack/tests/test_metrics/test_auc_aman.py
@@ -15,10 +15,15 @@ def test_auc_aman():
     items_pred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     y_pred = csr_matrix((values_pred, (users_pred, items_pred)), shape=(1, 10))
 
-    aucaman = AUCAMAN()
-    aucaman.calculate(y_true, y_pred)
+    values = [1, 1, 1, 1, 1, 1]
+    users = [0, 0, 0, 0, 0, 0]
+    items = [0, 1, 2, 3, 4, 7]
+    X = csr_matrix((values, (users, items)), shape=(1, 10))
 
-    manual_numerator = 9/9 + 7/9 + 6/9 + 2/9
+    aucaman = AUCAMAN()
+    aucaman.calculate(y_true, y_pred, X)
+
+    manual_numerator = 4/4 + 3/4 + 3/4 + 0/4
     manual_denominator = 4
 
     numpy.testing.assert_almost_equal(aucaman.value, manual_numerator / manual_denominator)
