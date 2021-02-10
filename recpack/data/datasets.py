55cedcf85d7899159ad3e1be5f38fca675ee7cf9
verachtertr <verachtertr@users.noreply.github.com>
2021-02-10 16:58:02 +0100

    Updated datasets module|

--- recpack/data/datasets.py
+++ recpack/data/datasets.py
@@ -1,116 +1,188 @@
 import numpy as np
+import os
 import pandas as pd
+from urllib.request import urlretrieve
+import zipfile
 
-
-from recpack.preprocessing.filters import MinItemsPerUser, MinUsersPerItem
+from recpack.preprocessing.filters import Filter, MinItemsPerUser, MinUsersPerItem
 from recpack.data.matrix import InteractionMatrix, to_binary
 from recpack.preprocessing.preprocessors import DataFramePreprocessor
 from recpack.util import to_tuple
 
-# TODO Refactor so that it downloads the datasets and names the fields consistently
+
+def _fetch_remote(url, filename):
+    # TODO: checksum?
+    urlretrieve(url, filename)
+    return filename
 
 
-def load_citeulike(self, path, min_ui=5, min_iu=1):
+# TODO: Timestamp column rename to seconds_since_epoch?
+# That would make clear what the data is, not sure though
+class Dataset:
 
-    # Different on purpose from the InteractionMatrix ones to avoid name colisions.
+    """
+    - Dataset downloading
+    - Dataset local load (if downloaded)
+    - Default preprocessing
+    - Add custom / specific Filters
+        + Create min_rating filter
+    - Load dataframe
+    - Load InteractionMatrix
+    """
+
+    USER_IX = "user_id"
+    ITEM_IX = "item_id"
+    TIMESTAMP_IX = "timestamp"
+
+    def __init__(self, filename, preprocess_default=True):
+        self.filename = filename
+        self.preprocessor = DataFramePreprocessor(
+            self.ITEM_IX, self.USER_IX, self.TIMESTAMP_IX, dedupe=False
+        )
+        if preprocess_default:
+            for f in self._default_filters:
+                self.add_filter(f)
+
+    @property
+    def _default_filters(self):
+        # Return a list of filters to be added to the preprocessor
+        # if default is enabled.
+
+        return [
+            MinItemsPerUser(3, self.ITEM_IX, self.USER_IX),
+            MinUsersPerItem(5, self.ITEM_IX, self.USER_IX),
+        ]
+
+    def add_filter(self, _filter: Filter):
+        self.preprocessor.add_filter(_filter)
+
+    def fetch_dataset(self, force=False):
+        """Check if dataset is present, if not download
+
+        if force => Overwrite
+        """
+        if not os.path.exists(self.filename) or force:
+            self._download_dataset()
+
+    def _download_dataset(self):
+        raise NotImplementedError("Should still be implemented")
+
+    def load_dataframe(self):
+        raise NotImplementedError("Needs to be implemented")
+
+    def load_interaction_matrix(self):
+        df = self.load_dataframe()
+
+        return self.preprocessor.process(df)
+
+
+class CiteULike(Dataset):
     USER_IX = "user"
     ITEM_IX = "item"
+    TIMESTAMP_IX = None
 
-    # TODO: Download if not at path
+    def _download_dataset(self):
+        DATASETURL = (
+            "https://raw.githubusercontent.com/js05212/citeulike-a/master/users.dat"
+        )
+        _fetch_remote(DATASETURL, self.filename)
 
-    # Load from path
-    u_i_pairs = []
-    with open(path, "r") as f:
+    def load_dataframe(self):
+        # This will download the dataset if necessary
+        self.fetch_dataset()
 
-        for user, line in enumerate(f.readlines()):
-            item_cnt = line.strip("\n").split(" ")[0]  # First element is a count
-            items = line.strip("\n").split(" ")[1:]
-            assert len(items) == int(item_cnt)
+        u_i_pairs = []
+        with open(self.filename, "r") as f:
 
-            for item in items:
-                assert item.isdecimal()  # Make sure the identifiers are correct.
-                u_i_pairs.append((user, item))
+            for user, line in enumerate(f.readlines()):
+                item_cnt = line.strip("\n").split(" ")[0]  # First element is a count
+                items = line.strip("\n").split(" ")[1:]
+                assert len(items) == int(item_cnt)
 
-    df = pd.DataFrame(u_i_pairs, columns=[USER_IX, ITEM_IX])
+                for item in items:
+                    assert item.isdecimal()  # Make sure the identifiers are correct.
+                    u_i_pairs.append((user, int(item)))
 
-    # Process to InteractionMatrix object
-    preprocessor = DataFramePreprocessor(ITEM_IX, USER_IX, dedupe=True)
-    preprocessor.add_filter(MinItemsPerUser(min_iu, USER_IX, ITEM_IX))
-    preprocessor.add_filter(MinUsersPerItem(min_ui, USER_IX, ITEM_IX))
-
-    return preprocessor.process(df)
+        # Rename columns to default ones ?
+        df = pd.DataFrame(u_i_pairs, columns=[self.USER_IX, self.ITEM_IX])
+        return df
 
 
-def load_ml_20m(path, min_rating=4, min_ui=5, min_iu=1):
+class MovieLens20M(Dataset):
     USER_IX = "userId"
-    ITEM_IX = "itemId"
+    ITEM_IX = "movieId"
     TIMESTAMP_IX = "timestamp"
     RATING_IX = "rating"
-    # Also has a rating, which we will ignore for now
-    # If we add a RatingMatrix it should be added as an option to load
 
-    # TODO: download from path
+    def _download_dataset(self):
+        DATASETURL = "http://files.grouplens.org/datasets/movielens/ml-25m.zip"
 
-    df = pd.read_csv(path)
+        # Get the director part of the file specified
+        dir_f = os.path.dirname(self.filename)
 
-    # keep only the rows that have a high enough rating
-    interactions = df[df[RATING_IX] >= min_rating]
+        # Download the zip into the directory
+        _fetch_remote(DATASETURL, os.path.join(dir_f, "ml-25m.zip"))
 
-    # Process to InteractionMatrix object
-    preprocessor = DataFramePreprocessor(
-        ITEM_IX, USER_IX, timestamp_ix=TIMESTAMP_IX, dedupe=True
-    )
-    preprocessor.add_filter(MinItemsPerUser(min_iu, USER_IX, ITEM_IX))
-    preprocessor.add_filter(MinUsersPerItem(min_ui, USER_IX, ITEM_IX))
+        # Extract the ratings file which we will use
+        with zipfile.ZipFile(os.path.join(dir_f, "ml-25m.zip"), "r") as zip_ref:
+            zip_ref.extract("ml-25m/ratings.csv", dir_f)
 
-    data = preprocessor.process(interactions)
-    return to_binary(data)
+        # Rename the ratings file to the specified filename
+        os.rename(os.path.join(dir_f, "ml-25m/ratings.csv"), self.filename)
+
+    def load_dataframe(self):
+        self.fetch_dataset()
+        df = pd.read_csv(self.filename)
+
+        return df
 
 
-def load_rsc_2015(path, nrows=None, min_ui=5, min_iu=1):
-    # On purpose different from the InteractionMatrix ones, to avoid column colision.
+class RecsysChallenge2015(Dataset):
     USER_IX = "session"
     ITEM_IX = "item"
     TIMESTAMP_IX = "timestamp"
 
-    df = pd.read_csv(
-        path,
-        names=[USER_IX, TIMESTAMP_IX, ITEM_IX],
-        dtype={USER_IX: np.int64, TIMESTAMP_IX: np.str, ITEM_IX: np.int64},
-        parse_dates=[TIMESTAMP_IX],
-        usecols=[0, 1, 2],
-        nrows=nrows,
-    )
+    def _download_dataset(self):
+        raise NotImplementedError(
+            "Recsys Challenge dataset should be downloaded manually, you can get it at: https://www.kaggle.com/chadgostopp/recsys-challenge-2015."
+        )
 
-    df[TIMESTAMP_IX] = (
-        df[TIMESTAMP_IX].astype(int) / 1e9
-    )  # pandas datetime -> seconds from epoch
+    # TODO: The original function had a parameter nrows,
+    # which allowed to just parse the first X rows.
+    # Do we want to add that functionality, feels a bit unneeded
 
-    # Process to dataframe
-    preprocessor = DataFramePreprocessor(
-        ITEM_IX, USER_IX, timestamp_ix=TIMESTAMP_IX, dedupe=True
-    )
+    @property
+    def _default_filters(self):
+        # Return a list of filters to be added to the preprocessor
+        # if default is enabled.
+        # TODO: this had a different preprocessing in the function compared to the others.
+        # Do we try to unify this, and make all defaults the same,
+        # or was there a particular reason for this one?
+        return [
+            MinItemsPerUser(3, self.USER_IX, self.ITEM_IX, count_duplicates=True),
+            MinUsersPerItem(5, self.USER_IX, self.ITEM_IX, count_duplicates=True),
+            MinItemsPerUser(3, self.USER_IX, self.ITEM_IX, count_duplicates=True),
+        ]
 
-    filter_users = MinItemsPerUser(
-        min_iu,
-        item_id=ITEM_IX,
-        user_id=USER_IX,
-        timestamp_id=TIMESTAMP_IX,
-        count_duplicates=True,
-    )
-    filter_items = MinUsersPerItem(
-        min_ui,
-        item_id=ITEM_IX,
-        user_id=USER_IX,
-        timestamp_id=TIMESTAMP_IX,
-        count_duplicates=True,
-    )
+    def load_dataframe(self):
+        self.fetch_dataset()
 
-    # add the filters
-    preprocessor.add_filter(filter_users)
-    preprocessor.add_filter(filter_items)
-    # Apply user filter a second time to remove users
-    # with too few items in their history after the items filter.
-    preprocessor.add_filter(filter_users)
+        df = pd.read_csv(
+            self.filename,
+            names=[self.USER_IX, self.TIMESTAMP_IX, self.ITEM_IX],
+            dtype={
+                self.USER_IX: np.int64,
+                self.TIMESTAMP_IX: np.str,
+                self.ITEM_IX: np.int64,
+            },
+            parse_dates=[self.TIMESTAMP_IX],
+            usecols=[0, 1, 2],
+            # nrows=nrows,
+        )
 
-    return preprocessor.process(df)
+        # Adapt timestamp, this makes it so the timestamp is always seconds since epoch
+        df[self.TIMESTAMP_IX] = (
+            df[self.TIMESTAMP_IX].astype(int) / 1e9
+        )  # pandas datetime -> seconds from epoch
+
+        return df
---
+++ recpack/tests/test_datasets/test_datasets.py
@@ -0,0 +1,129 @@
+import os
+import pytest
+
+from recpack.data import datasets
+from recpack.preprocessing.filters import NMostPopular
+
+
+def test_fetch_dataset():
+    file_name = "/tmp/citeulike.dat"
+    # We'll test using citeulike
+    d = datasets.CiteULike("/tmp/citeulike.dat")
+
+    # The file should not exist yet.
+    assert not os.path.exists(file_name)
+
+    # Fetch dataset will download the file
+    d.fetch_dataset()
+
+    # Now file should exist
+    assert os.path.exists(file_name)
+
+    # This should load the full dataframe.
+    df = d.load_dataframe()
+    assert df.shape == (204986, 2)
+
+    # We'll overwrite the file with some other content in the same format
+    with open(file_name, "w") as f:
+        f.write("2 1 2\n")
+        f.write("1 2")
+
+    # Dataframe gets reloaded from file
+    df2 = d.load_dataframe()
+    assert df2.shape == (3, 2)
+
+    # Fetching with the file already existing does not overwrite the file
+    d.fetch_dataset()
+
+    # No changes in dataframe, since file was not changed
+    df2_bis = d.load_dataframe()
+    assert df2_bis.shape == df2.shape
+
+    # With the force=True parameter the dataset will be downloaded,
+    # overwriting of the already existing file
+    d.fetch_dataset(force=True)
+
+    # Dataframe should be the same as the first download
+    df_bis = d.load_dataframe()
+    assert df_bis.shape == df.shape
+
+    # Clean up again
+    os.remove(file_name)
+
+
+def test_add_filter():
+    path_to_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "datasets/citeulike_sample.dat"
+    )
+
+    d = datasets.CiteULike(path_to_file)
+
+    d.add_filter(NMostPopular(3, d.ITEM_IX, d.USER_IX))
+
+    data = d.load_interaction_matrix()
+
+    assert data.shape[1] == 3
+
+
+def test_citeulike():
+    # To get sample we used head -1000 users.dat
+    path_to_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "datasets/citeulike_sample.dat"
+    )
+
+    d = datasets.CiteULike(path_to_file)
+
+    df = d.load_dataframe()
+    assert (df.columns == ["user", "item"]).all()
+
+    assert df.shape == (36872, 2)
+    assert df.user.nunique() == 1000
+    assert df.item.nunique() == 13689
+
+    data = d.load_interaction_matrix()
+
+    assert data.shape == (963, 1748)
+
+
+def test_movielens20m():
+    # To get sample we used head -10000 ratings.csv
+    path_to_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "datasets/ml-25m_sample.csv"
+    )
+
+    d = datasets.MovieLens20M(path_to_file)
+
+    df = d.load_dataframe()
+    assert (df.columns == ["userId", "movieId", "rating", "timestamp"]).all()
+
+    assert df.shape == (9999, 4)
+    assert df.userId.nunique() == 75
+    assert df.movieId.nunique() == 3287
+
+    data = d.load_interaction_matrix()
+
+    assert data.shape == (75, 573)
+
+
+def test_recsys_challenge_2015():
+    # To get sample we used head -1000 ratings.csv
+    path_to_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "datasets/yoochoose-clicks_sample.dat",
+    )
+
+    d = datasets.RecsysChallenge2015(path_to_file)
+
+    df = d.load_dataframe()
+    assert (df.columns == ["session", "timestamp", "item"]).all()
+    assert df.shape == (1000, 3)
+    assert df.session.nunique() == 272
+    assert df.item.nunique() == 570
+
+    data = d.load_interaction_matrix()
+
+    assert data.shape == (15, 20)
+
+    # We can't just download this dataset, since it requires Kaggle access
+    with pytest.raises(NotImplementedError):
+        d._download_dataset()
