4627eb93e359bf5222f90ab3e16f82a2ee48c330
verachtertr <verachtertr@users.noreply.github.com>
2021-02-22 17:23:30 +0100

    Refactored the preprocessing module, parameter names changed + unused parameters removed + moved data tests to a test_data module for easy testing

--- recpack/preprocessing/filters.py
+++ recpack/preprocessing/filters.py
@@ -10,11 +10,6 @@ import pandas as pd
 
 
 class Filter(ABC):
-    def __init__(self, item_id, user_id, timestamp_id=None):
-        self.user_id = user_id
-        self.item_id = item_id
-        self.timestamp_id = timestamp_id
-
     def apply_all(self, *dfs: pd.DataFrame) -> List[pd.DataFrame]:
         ret = list()
         for df in dfs:
@@ -32,9 +27,7 @@ class Filter(ABC):
         raise NotImplementedError
 
     def __str__(self):
-        attrs = self.__dict__.copy()
-        for k in ["user_id", "item_id", "timestamp_id"]:
-            del attrs[k]
+        attrs = self.__dict__
         return f"{self.__class__.__name__}({', '.join((f'{k}={v}' for k, v in attrs.items()))})"
 
 
@@ -42,9 +35,8 @@ class MinUsersPerItem(Filter):
     def __init__(
         self,
         min_users_per_item: int,
-        item_id: str,
-        user_id: str,
-        # timestamp_id: str = None,
+        item_ix: str,
+        user_ix: str,
         count_duplicates: bool = False,
     ):
         """
@@ -52,36 +44,35 @@ class MinUsersPerItem(Filter):
 
         :param min_users_per_item: Minimum number of users required.
         :type min_users_per_item: int
-        :param item_id: Name of the column in which item identifiers are listed.
-        :type item_id: str
-        :param user_id: Name of the column in which user identifiers are listed.
-        :type user_id: str
-        :param timestamp_id: Name of the column in which timestamps are listed, defaults to None
-        :type timestamp_id: str, optional
+        :param item_ix: Name of the column in which item identifiers are listed.
+        :type item_ix: str
+        :param user_ix: Name of the column in which user identifiers are listed.
+        :type user_ix: str
         :param count_duplicates: Count multiple interactions with the same user, defaults to False
         :type count_duplicates: bool
         """
+        self.item_ix = item_ix
+        self.user_ix = user_ix
+
         self.min_ui = min_users_per_item
         self.count_duplicates = count_duplicates
 
-        super().__init__(item_id, user_id, timestamp_id=timestamp_id)
-
     def apply(self, df):
         iids = (
-            df[self.item_id]
+            df[self.item_ix]
             if self.count_duplicates
-            else df.drop_duplicates([self.user_id, self.item_id])[self.item_id]
+            else df.drop_duplicates([self.user_ix, self.item_ix])[self.item_ix]
         )
         cnt_users_per_item = iids.value_counts()
         items_of_interest = list(
             cnt_users_per_item[cnt_users_per_item >= self.min_ui].index
         )
 
-        return df[df[self.item_id].isin(items_of_interest)]
+        return df[df[self.item_ix].isin(items_of_interest)]
 
 
 class NMostPopular(Filter):
-    def __init__(self, N: int, item_id: str, user_id: str, timestamp_id=None):
+    def __init__(self, N: int, item_ix: str):
         """
         Retain only the N most popular items.
         Note: All interactions count, also if a user interacted with the same item
@@ -89,58 +80,57 @@ class NMostPopular(Filter):
 
         :param N: Number of items to retain.
         :type N: int
-        :param item_id: Name of the column in which item identifiers are listed.
-        :type item_id: str
+        :param item_ix: Name of the column in which item identifiers are listed.
+        :type item_ix: str
         :param user_id: Name of the column in which user identifiers are listed.
         :type user_id: str
         :param timestamp_id: Name of the column in which timestamps are listed, defaults to None
         :type timestamp_id: str, optional
         """
         self.N = N
-
-        super().__init__(item_id, user_id, timestamp_id=timestamp_id)
+        self.item_ix = item_ix
 
     def apply(self, df):
-        cnt_users_per_item = df[self.item_id].value_counts(sort=True, ascending=False)
+        cnt_users_per_item = df[self.item_ix].value_counts(sort=True, ascending=False)
 
         items_of_interest = list(cnt_users_per_item[0 : self.N].index)
 
-        return df[df[self.item_id].isin(items_of_interest)]
+        return df[df[self.item_ix].isin(items_of_interest)]
 
 
 class NMostRecent(Filter):
     """Select only events on the N most recently visited items.
 
+    If there is a tie, the first occuring item in the dataframe is retained.
+
     :param N: Number of items to retain.
     :type N: int
-    :param item_id: Name of the column in which item identifiers are listed.
-    :type item_id: str
-    :param user_id: Name of the column in which user identifiers are listed.
-    :type user_id: str
-    :param timestamp_id: Name of the column in which timestamps are listed, defaults to None
-    :type timestamp_id: str, optional
+    :param item_ix: Name of the column in which item identifiers are listed.
+    :type item_ix: str
+    :param timestamp_ix: Name of the column in which timestamps are listed, defaults to None
+    :type timestamp_ix: str, optional
     """
 
-    def __init__(self, N: int, item_id: str, user_id: str, timestamp_id: str):
+    def __init__(self, N: int, item_ix: str, timestamp_ix: str):
         self.N = N
-        super().__init__(item_id, user_id, timestamp_id=timestamp_id)
+        self.item_ix = item_ix
+        self.timestamp_ix = timestamp_ix
 
     def apply(self, df):
-        sorted_df = df[[self.item_id, self.timestamp_id]].sort_values(
-            by=self.timestamp_id, ascending=False
+        sorted_df = df[[self.item_ix, self.timestamp_ix]].sort_values(
+            by=self.timestamp_ix, ascending=False
         )
-        ids = sorted_df[[self.item_id]].drop_duplicates()
+        ids = sorted_df[[self.item_ix]].drop_duplicates()
 
-        return df[df[self.item_id].isin(ids[: self.N][self.item_id])]
+        return df[df[self.item_ix].isin(ids[: self.N][self.item_ix])]
 
 
 class MinItemsPerUser(Filter):
     def __init__(
         self,
         min_items_per_user: int,
-        item_id: str,
-        user_id: str,
-        timestamp_id: str = None,
+        item_ix: str,
+        user_ix: str,
         count_duplicates: bool = False,
     ):
         """
@@ -148,32 +138,31 @@ class MinItemsPerUser(Filter):
 
         :param min_items_per_user: Minimum number of items required.
         :type min_items_per_user: int
-        :param item_id: Name of the column in which item identifiers are listed.
-        :type item_id: str
-        :param user_id: Name of the column in which user identifiers are listed.
-        :type user_id: str
-        :param timestamp_id: Name of the column in which timestamps are listed, defaults to None
-        :type timestamp_id: str, optional
+        :param item_ix: Name of the column in which item identifiers are listed.
+        :type item_ix: str
+        :param user_ix: Name of the column in which user identifiers are listed.
+        :type user_ix: str
         :param count_duplicates: Count multiple interactions with the same item, defaults to False
         :type count_duplicates: bool
         """
         self.min_iu = min_items_per_user
         self.count_duplicates = count_duplicates
 
-        super().__init__(item_id, user_id, timestamp_id=timestamp_id)
+        self.item_ix = item_ix
+        self.user_ix = user_ix
 
     def apply(self, df):
         uids = (
-            df[self.user_id]
+            df[self.user_ix]
             if self.count_duplicates
-            else df.drop_duplicates([self.user_id, self.item_id])[self.user_id]
+            else df.drop_duplicates([self.user_ix, self.item_ix])[self.user_ix]
         )
         cnt_items_per_user = uids.value_counts()
         users_of_interest = list(
             cnt_items_per_user[cnt_items_per_user >= self.min_iu].index
         )
 
-        return df[df[self.user_id].isin(users_of_interest)]
+        return df[df[self.user_ix].isin(users_of_interest)]
 
 
 class MinRating(Filter):
@@ -183,21 +172,17 @@ class MinRating(Filter):
 
     After filtering, the rating_ix column will also be removed from the dataframe.
 
+    :param min_rating: The minimum rating for a rating to be considered an interaction.
+    :type min_rating: int
     :param rating_ix: the column that will contain ratings in the dataframe
     :type str:
-    :param min_rating: The minimum rating for a rating to be considered an interaction, defaults to 1
-    :type min_rating: int, optional
     """
 
     def __init__(
         self,
+        min_rating: int,
         rating_ix: str,
-        item_id: str,
-        user_id: str,
-        timestamp_id: str = None,
-        min_rating: int = 1,
     ):
-        super().__init__(item_id, user_id, timestamp_id)
         self.rating_ix = rating_ix
         self.min_rating = min_rating
 
@@ -210,23 +195,25 @@ class Deduplicate(Filter):
 
     Removes all but one of each user-item pair in the dataframe.
     If timestamps are available, the first interaction is kept.
-    :param item_id: Name of the column in which item identifiers are listed.
-    :type item_id: str
-    :param user_id: Name of the column in which user identifiers are listed.
-    :type user_id: str
-    :param timestamp_id: Name of the column in which timestamps are listed, defaults to None
-    :type timestamp_id: str, optional
+    :param item_ix: Name of the column in which item identifiers are listed.
+    :type item_ix: str
+    :param user_ix: Name of the column in which user identifiers are listed.
+    :type user_ix: str
+    :param timestamp_ix: Name of the column in which timestamps are listed, defaults to None
+    :type timestamp_ix: str, optional
     """
 
     def __init__(
         self,
-        item_id: str,
-        user_id: str,
-        timestamp_id: str = None,
+        item_ix: str,
+        user_ix: str,
+        timestamp_ix: str = None,
     ):
-        super().__init__(item_id, user_id, timestamp_id)
+        self.item_ix = item_ix
+        self.user_ix = user_ix
+        self.timestamp_ix = timestamp_ix
 
     def apply(self, df):
-        if self.timestamp_id is not None:
-            df = df.sort_values(self.timestamp_id)
-        return df.drop_duplicates([self.user_id, self.item_id], keep="first")
+        if self.timestamp_ix is not None:
+            df = df.sort_values(self.timestamp_ix)
+        return df.drop_duplicates([self.user_ix, self.item_ix], keep="first")
