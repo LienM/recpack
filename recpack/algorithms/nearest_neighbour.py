8888feb5d26814a52a1cff929662cdc6eca0804c
verachtertr <verachtertr@users.noreply.github.com>
2021-03-03 13:28:12 +0100

    NotItemKNN merged into ItemKNN by using similarity option

--- recpack/algorithms/nearest_neighbour.py
+++ recpack/algorithms/nearest_neighbour.py
@@ -9,7 +9,7 @@ from recpack.algorithms.base import TopKSimilarityMatrixAlgorithm
 
 
 class ItemKNN(TopKSimilarityMatrixAlgorithm):
-    def __init__(self, K=200, normalize=False):
+    def __init__(self, K=200, normalize=False, similarity: str = "cosine"):
         """Construct an ItemKNN model. Before use make sure to fit the model.
         The K parameter defines the how much best neighbours are kept for each item.
 
@@ -17,65 +17,9 @@ class ItemKNN(TopKSimilarityMatrixAlgorithm):
         """
         super().__init__(K)
         self.normalize = normalize
+        self.similarity = similarity
 
-    def fit(self, X: Matrix):
-        """Fit a cosine similarity matrix from item to item"""
-        X = to_csr_matrix(X, binary=True)
-
-        # Do the cosine similarity computation here, this way we can set the diagonal to zero
-        # to avoid self recommendation
-        # X.T otherwise we are doing a user KNN
-        item_cosine_similarities_ = cosine_similarity(X.T, dense_output=False)
-
-        item_cosine_similarities_.setdiag(0)
-        # Set diagonal to 0, because we don't want to support self similarity
-
-        # resolve top K per item
-        # Get indices of top K items per item
-        indices = [
-            (i, j)
-            for i, best_items_row in enumerate(
-                np.argpartition(item_cosine_similarities_.toarray(), -self.K)
-            )
-            for j in best_items_row[-self.K :]
-        ]
-        # Create a mask matrix which will be pointwise multiplied with the
-        # similarity matrix.
-        mask = scipy.sparse.csr_matrix(
-            ([1 for i in range(len(indices))], (list(zip(*indices))))
-        )
-
-        item_cosine_similarities_ = item_cosine_similarities_.multiply(mask)
-
-        if self.normalize:
-            # normalize per row
-            row_sums = item_cosine_similarities_.sum(axis=1)
-            item_cosine_similarities_ = item_cosine_similarities_ / row_sums
-            item_cosine_similarities_ = scipy.sparse.csr_matrix(
-                item_cosine_similarities_
-            )
-
-        self.similarity_matrix_ = item_cosine_similarities_
-        self._check_fit_complete()
-        return self
-
-
-class NotItemKNN(TopKSimilarityMatrixAlgorithm):
-    """
-    TODO: Figure out what this code is actually implementing. It is not cosine similarity
-    It does seem to work fine though.
-
-    I think I figured it out :p -> These seem to be conditional probabilities
-    sup(i,j) / sup(i)
-    """
-
-    """Construct an ItemKNN model. Before use make sure to fit the model.
-    The K parameter defines the how much best neighbours are kept for each item."""
-
-    def fit(self, X: Matrix):
-        """Fit a cosine similarity matrix from item to item"""
-        X = to_csr_matrix(X, binary=True)
-
+    def _compute_conditional_probability(self, X):
         co_mat = X.T @ X
         # Do the cosine similarity computation here, this way we can set the diagonal to zero
         # to avoid self recommendation
@@ -83,17 +27,39 @@ class NotItemKNN(TopKSimilarityMatrixAlgorithm):
 
         # This has all item-cosine similarities. Now we should probably set N-K
         # to zero
-        item_cosine_similarities_ = A @ co_mat
+        item_cosine_similarities = A @ co_mat
 
         # Set diagonal to 0, because we don't support self similarity
-        item_cosine_similarities_.setdiag(0)
+        item_cosine_similarities.setdiag(0)
+
+        return item_cosine_similarities
+
+    def _compute_cosine(self, X):
+        # Do the cosine similarity computation here, this way we can set the diagonal to zero
+        # to avoid self recommendation
+        # X.T otherwise we are doing a user KNN
+        item_cosine_similarities = cosine_similarity(X.T, dense_output=False)
+
+        item_cosine_similarities.setdiag(0)
+        # Set diagonal to 0, because we don't want to support self similarity
+
+        return item_cosine_similarities
+
+    def fit(self, X: Matrix):
+        """Fit a cosine similarity matrix from item to item"""
+        X = to_csr_matrix(X, binary=True)
+
+        if self.similarity == "cosine":
+            item_cosine_similarities = self._compute_cosine(X)
+        elif self.similarity == "conditional_probability":
+            item_cosine_similarities = self._compute_conditional_probability(X)
 
         # resolve top K per item
         # Get indices of top K items per item
         indices = [
             (i, j)
             for i, best_items_row in enumerate(
-                np.argpartition(item_cosine_similarities_.toarray(), -self.K)
+                np.argpartition(item_cosine_similarities.toarray(), -self.K)
             )
             for j in best_items_row[-self.K :]
         ]
@@ -102,8 +68,15 @@ class NotItemKNN(TopKSimilarityMatrixAlgorithm):
         mask = scipy.sparse.csr_matrix(
             ([1 for i in range(len(indices))], (list(zip(*indices))))
         )
-        item_cosine_similarities_ = item_cosine_similarities_.multiply(mask)
 
-        self.similarity_matrix_ = item_cosine_similarities_
+        item_cosine_similarities = item_cosine_similarities.multiply(mask)
+
+        if self.normalize:
+            # normalize per row
+            row_sums = item_cosine_similarities.sum(axis=1)
+            item_cosine_similarities = item_cosine_similarities / row_sums
+            item_cosine_similarities = scipy.sparse.csr_matrix(item_cosine_similarities)
+
+        self.similarity_matrix_ = item_cosine_similarities
         self._check_fit_complete()
         return self
--- recpack/tests/test_algorithms/test_nearest_neighbour.py
+++ recpack/tests/test_algorithms/test_nearest_neighbour.py
@@ -1,16 +1,34 @@
-import recpack.algorithms
-import scipy.sparse
 import math
 import numpy
+import pytest
+import scipy.sparse
+
+from recpack.algorithms import ItemKNN
 
 
-def test_item_knn():
+@pytest.fixture(scope="function")
+def data():
     values = [1] * 7
     users = [0, 0, 1, 1, 2, 2, 2]
     items = [1, 2, 0, 2, 0, 1, 2]
-    data = scipy.sparse.csr_matrix((values, (users, items)), shape=(4, 3))
+    d = scipy.sparse.csr_matrix((values, (users, items)), shape=(4, 3))
 
-    algo = recpack.algorithms.algorithm_registry.get("itemKNN")(K=2)
+    return d
+
+
+@pytest.fixture(scope="function")
+def data_empty_col():
+    values = [1] * 5
+    users = [0, 0, 1, 1, 2]
+    items = [1, 2, 2, 1, 2]
+    d = scipy.sparse.csr_matrix((values, (users, items)))
+
+    return d
+
+
+def test_item_knn(data):
+
+    algo = ItemKNN(K=2)
 
     algo.fit(data)
 
@@ -39,30 +57,33 @@ def test_item_knn():
     numpy.testing.assert_almost_equal(result.toarray(), expected_out)
 
 
-def test_item_knn_normalize():
-    values = [1] * 7
-    users = [0, 0, 1, 1, 2, 2, 2]
-    items = [1, 2, 0, 2, 0, 1, 2]
-    data = scipy.sparse.csr_matrix((values, (users, items)), shape=(4, 3))
+def test_item_knn_normalize(data):
 
-    algo = recpack.algorithms.algorithm_registry.get("itemKNN")(K=2, normalize=True)
+    algo = ItemKNN(K=2, normalize=True)
 
     algo.fit(data)
 
     numpy.testing.assert_array_almost_equal(algo.similarity_matrix_.sum(axis=1), 1)
 
 
-def test_item_knn_empty_col():
-    values = [1] * 5
-    users = [0, 0, 1, 1, 2]
-    items = [1, 2, 2, 1, 2]
-    data = scipy.sparse.csr_matrix((values, (users, items)))
-    algo = recpack.algorithms.algorithm_registry.get("itemKNN")(K=2)
+def test_item_knn_empty_col(data_empty_col):
+    algo = ItemKNN(K=2)
 
-    algo.fit(data)
+    algo.fit(data_empty_col)
     expected_similarities = numpy.array(
         [[0.0, 0.0, 0.0], [0.0, 0, 2 / math.sqrt(6)], [0.0, 2 / math.sqrt(6), 0]]
     )
+    numpy.testing.assert_almost_equal(
+        algo.similarity_matrix_.toarray(), expected_similarities
+    )
+
+
+def test_item_knn_conditional_probability(data):
+    algo = ItemKNN(K=2, similarity="conditional_probability")
+
+    algo.fit(data)
+    expected_similarities = numpy.array([[0, 0.5, 1], [0.5, 0, 1], [2 / 3, 2 / 3, 0]])
+
     numpy.testing.assert_almost_equal(
         algo.similarity_matrix_.toarray(), expected_similarities
     )
