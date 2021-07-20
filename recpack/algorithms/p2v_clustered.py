4397f7155a98f93b49a7c1d77ed5636a64526e61
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2021-07-20 18:47:37 +0200

    Conver another tensor to cpu

--- recpack/algorithms/p2v_clustered.py
+++ recpack/algorithms/p2v_clustered.py
@@ -241,7 +241,7 @@ class Prod2VecClustered(Prod2Vec):
         :return: array with a cluster label for every item. Shape = (|I|,)
         :rtype: np.array
         """
-        embedding = self.model_.input_embeddings.weight.detach().numpy()
+        embedding = self.model_.input_embeddings.weight.cpu().detach().numpy()
         kmeans = KMeans(self.num_clusters)
         cluster_assignments = kmeans.fit_predict(embedding)
         return cluster_assignments
