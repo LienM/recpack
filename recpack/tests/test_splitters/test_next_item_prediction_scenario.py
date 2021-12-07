36224208081d1ed46812fe4e605b0d65a6b5b316
verachtertr <verachtertr@users.noreply.github.com>
2021-12-07 16:52:26 +0100

    Changed some of the tests for next item prediction to more truthfully check the values

--- recpack/tests/test_splitters/test_next_item_prediction_scenario.py
+++ recpack/tests/test_splitters/test_next_item_prediction_scenario.py
@@ -16,9 +16,9 @@ def test_next_item_prediction_split(data_m_small):
     train_users = set(train.indices[0])
     test_data_out_users = set(test_data_out.indices[0])
     test_data_in_users = set(test_data_in.indices[0])
-    assert train_users.intersection(test_data_in_users)
-    assert train_users.intersection(test_data_out_users)
-    # Length asserts: every user should have exactly one data_out and data_in entry
+    assert train_users.difference(test_data_in_users) == set()
+    assert train_users.difference(test_data_out_users) == set()
+    # Length asserts: every user should have exactly one data_out and data_in row
     assert test_data_out.shape[0] == im._df["uid"].nunique()
     assert test_data_in.shape[0] == im._df["uid"].nunique()
 
@@ -51,10 +51,10 @@ def test_next_item_prediction_split_w_validation(data_m_small):
     test_data_in_users = set(test_data_in.indices[0])
     val_data_out_users = set(val_data_out.indices[0])
     val_data_in_users = set(val_data_in.indices[0])
-    assert train_users.intersection(test_data_in_users)
-    assert train_users.intersection(test_data_out_users)
-    assert train_users.intersection(val_data_in_users)
-    assert train_users.intersection(val_data_out_users)
+    assert train_users.difference(test_data_in_users) == set()
+    assert train_users.difference(test_data_out_users) == set()
+    assert train_users.difference(val_data_in_users) == set()
+    assert train_users.difference(val_data_out_users) == set()
     # Length asserts: every user should have exactly one _out and _in entry
     assert test_data_out.shape[0] == im._df["uid"].nunique()
     assert test_data_in.shape[0] == im._df["uid"].nunique()
@@ -71,6 +71,7 @@ def test_next_item_prediction_split_w_validation(data_m_small):
         assert test_data_out.values[u, sorted_item_history[-1]] == 1
         assert test_data_in.values[u, sorted_item_history[-2]] == 1
         assert val_data_out.values[u, sorted_item_history[-2]] == 1
+        assert val_data_in.values[u, sorted_item_history[-3]] == 1
 
         assert (
             train.values[u].sum() == len(sorted_item_history) - 2
@@ -90,8 +91,8 @@ def test_next_item_prediction_split_w_n_most_recent(data_m_small, n_most_recent)
     train_users = set(train.indices[0])
     test_data_out_users = set(test_data_out.indices[0])
     test_data_in_users = set(test_data_in.indices[0])
-    assert train_users.intersection(test_data_in_users)
-    assert train_users.intersection(test_data_out_users)
+    assert train_users.difference(test_data_in_users) == set()
+    assert train_users.difference(test_data_out_users) == set()
     # Length asserts: every user should have exactly one data_out and data_in entry
     assert test_data_out.shape[0] == im._df["uid"].nunique()
     assert test_data_in.shape[0] == im._df["uid"].nunique()
@@ -133,10 +134,10 @@ def test_next_item_prediction_split_w_validation_w_n_most_recent(
     test_data_in_users = set(test_data_in.indices[0])
     val_data_out_users = set(val_data_out.indices[0])
     val_data_in_users = set(val_data_in.indices[0])
-    assert train_users.intersection(test_data_in_users)
-    assert train_users.intersection(test_data_out_users)
-    assert train_users.intersection(val_data_in_users)
-    assert train_users.intersection(val_data_out_users)
+    assert train_users.difference(test_data_in_users) == set()
+    assert train_users.difference(test_data_out_users) == set()
+    assert train_users.difference(val_data_in_users) == set()
+    assert train_users.difference(val_data_out_users) == set()
     # Length asserts: every user should have exactly one _out and _in entry
     assert test_data_out.shape[0] == im._df["uid"].nunique()
     assert test_data_in.shape[0] == im._df["uid"].nunique()
