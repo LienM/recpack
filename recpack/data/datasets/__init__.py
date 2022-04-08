2ed7de5f5ab1685f9e4d6aea796abbd0ea579d52
bjornrexhepi99 <bjornrexhepi99@gmail.com>
2022-04-08 11:26:51 +0200

    Renaming 2 of the dataset files and checking the insertion of the final newline

--- recpack/data/datasets/__init__.py
+++ recpack/data/datasets/__init__.py
@@ -2,7 +2,7 @@ from recpack.data.datasets.dataset import Dataset
 from recpack.data.datasets.cite_u_like import CiteULike
 from recpack.data.datasets.cosmetics_shop import CosmeticsShop
 from recpack.data.datasets.dummy_dataset import DummyDataset
-from recpack.data.datasets.movie_lens25m import MovieLens25M
-from recpack.data.datasets.recsys_challenge2015 import RecsysChallenge2015
+from recpack.data.datasets.movie_lens import MovieLens25M
+from recpack.data.datasets.recsys_challenge import RecsysChallenge2015
 from recpack.data.datasets.retail_rocket import RetailRocket
 from recpack.data.datasets.thirty_music_sessions import ThirtyMusicSessions
