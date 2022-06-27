04b7f254a0049be22ac93c1b687d5edbfbd9411d
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2022-06-27 13:51:30 +0200

    Try to autowrap tables

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -59,6 +59,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for MathJax --------
 mathjax_config = {
     "extensions": ["tex2jax.js"],
