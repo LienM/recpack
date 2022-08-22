5982b22c81a2f82a5bb25d2c57f1caaefd342081
verachtertr <verachtertr@users.noreply.github.com>
2022-08-22 14:20:45 +0200

    replaced linear decay with linear decay steeper which is more generic

--- recpack/algorithms/time_aware_item_knn/base.py
+++ recpack/algorithms/time_aware_item_knn/base.py
@@ -21,7 +21,6 @@ from recpack.algorithms.time_aware_item_knn.decay_functions import (
     exponential_decay,
     log_decay,
     linear_decay,
-    linear_decay_steeper,
     concave_decay,
     convex_decay,
     inverse_decay,
@@ -76,10 +75,9 @@ class TARSItemKNN(TopKItemSimilarityMatrixAlgorithm):
         "exponential": exponential_decay,
         "log": log_decay,
         "linear": linear_decay,
-        "linear_steeper": linear_decay_steeper,
         "concave": concave_decay,
         "convex": convex_decay,
-        "unity": lambda x: x,
+        "identity": lambda x: x,
         "inverse": inverse_decay,
     }
 
--- recpack/algorithms/time_aware_item_knn/decay_functions.py
+++ recpack/algorithms/time_aware_item_knn/decay_functions.py
@@ -117,33 +117,6 @@ def linear_decay(age_arr: np.array, decay: float, max_age: float = None):
 
         1 - (\\frac{x}{max_{x \\in X_u}(x)}) \\alpha
 
-    where alpha is the decay parameter.
-
-    :param age_array: array of age of events which will be decayed.
-    :type age_array: np.array
-    :param decay: The decay parameter, should be in the [0, 1] interval.
-    :type decay: float
-    :param max_age: The max age normalisation parameter
-    :type max_age: float, optional
-    :returns: The decayed input time array. Larger values in the original array will have lower values.
-    :rtype: np.array
-    """
-    if not (0 <= decay <= 1):
-        raise ValueError(f"decay parameter = {decay} is not in the supported range: [0, 1].")
-
-    max_age = max_age if max_age is not None else age_arr.max()
-    return 1 - (age_arr / max_age) * decay
-
-
-def linear_decay_steeper(age_arr: np.array, decay: float, max_age: float = None):
-    """Applies a linear decay function.
-
-    For each value x in the ``age_arr`` the decayed value is computed as
-
-    .. math::
-
-        1 - (\\frac{x}{max_{x \\in X_u}(x)}) \\alpha
-
     where alpha is the decay parameter. If the decayed value is below 0, it is set to 0.
 
     :param age_array: array of age of events which will be decayed.
