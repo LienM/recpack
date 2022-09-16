3227313e59059b2ae52d9709a1b7ea108419c1bd
Robin Verachtert <verachtertr@gmail.com>
2022-09-16 14:22:31 +0000

    Release/0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="recpack",
-    version="0.3.1",
+    version="0.3.2",
     python_requires=">=3.8",
     packages=find_packages(),
     install_requires=[
