9cf23b402fef84a5a2e0a79fdc1b00926ddaa878
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2023-03-29 15:42:35 +0200

    Clean up docs Liu2021

--- recpack/algorithms/time_aware_item_knn/liu_2012.py
+++ recpack/algorithms/time_aware_item_knn/liu_2012.py
@@ -15,24 +15,27 @@ from recpack.matrix import InteractionMatrix
 
 
 class LiuDecay(DecayFunction):
-    """Computes a log based alpha function.
+    """Computes a logarithmic decay function.
 
     Every x in the `time_array` is discounted to:
     .. math::
 
-        log_\\alpha ((\\alpha-1)x + 1) + 1
+        f(x) = log_\\alpha ((\\alpha-1)x + 1) + 1
 
     Where :math:`\\alpha` is the decay parameter.
+
+    :param decay: The decay parameter, should be in the ]1, inf[ interval.
+    :type decay: float
     """
 
     @classmethod
-    def validate_decay(cls, alpha: float):
-        if not alpha > 1:
-            raise ValueError(f"Decay parameter alpha = {alpha} is not in the supported range: ]1, inf [")
+    def validate_decay(cls, decay: float):
+        if not decay > 1:
+            raise ValueError(f"Decay parameter = {decay} is not in the supported range: ]1, inf [")
 
-    def __init__(self, alpha: float):
-        self.validate_decay(alpha)
-        self.alpha = alpha
+    def __init__(self, decay: float):
+        self.validate_decay(decay)
+        self.decay = decay
 
     def __call__(self, time_array: ArrayLike) -> ArrayLike:
         """Apply decay.
@@ -40,37 +43,44 @@ class LiuDecay(DecayFunction):
         Values should be in the [0, 1] interval.
         :type time_array: np.array
         """
-        return (np.log(((self.alpha - 1) * time_array) + 1) / np.log(self.alpha)) + 1
+        return (np.log(((self.decay - 1) * time_array) + 1) / np.log(self.decay)) + 1
 
 
 class TARSItemKNNLiu2012(TARSItemKNN):
     """Time aware variant of ItemKNN which uses a logarithmic decay function.
 
-    Algorithm as described in 
-    Y. Liu, Z. Xu, B. Shi and B. Zhang, 
-    "Time-Based K-nearest Neighbor Collaborative Filtering," 
-    2012 IEEE 12th International Conference on Computer and Information Technology, 
+    Algorithm as described in
+    Y. Liu, Z. Xu, B. Shi and B. Zhang,
+    "Time-Based K-nearest Neighbor Collaborative Filtering,"
+    2012 IEEE 12th International Conference on Computer and Information Technology,
     Chengdu, China, 2012, pp. 1061-1065, doi: 10.1109/CIT.2012.217.
 
-    Weights are computed based on their position in a user's history. 1st item visited by a user gets value 1,
-    last item visited gets value 2. The decay follows a logarithmic function.
+    Weights are computed based on their position in a user's history.
+    The first item visited by a user gets value 1,
+    last item visited gets value 2.
+    The decay follows a logarithmic function:
 
     .. math::
 
-        \\log_\\alpha ((\\alpha-1)x + 1) + 1
+        \\Gamma(x) = \\log_\\alpha ((\\alpha-1)x + 1) + 1
+
+    where alpha is the decay scaling parameter,
+    and x is the time between the maximal timestamp in the matrix
+    and the timestamp of the event.
 
     :param K: The number of neighbours to keep per item.
     :type K: int
     :param decay: The parameter of the logarithmic decay function. Defaults to 2.
+    :type decay: float
     """
 
-    def __init__(self, K=200, decay=2):
-        # TODO Add decay function to the list so it can be created normally
-        super().__init__(K=K, fit_decay=decay, predict_decay=decay, decay_function="log", similarity="cosine")
-        self.decay_function = LiuDecay(decay)
+    DECAY_FUNCTIONS = {"liu": LiuDecay}
+
+    def __init__(self, K: int = 200, decay: float = 2.0):
+        super().__init__(K=K, fit_decay=decay, predict_decay=decay, decay_function="liu", similarity="cosine")
         self.decay = decay
 
-    def _add_decay_to_interaction_matrix(self, X: InteractionMatrix) -> csr_matrix:
+    def _add_decay_to_interaction_matrix(self, X: InteractionMatrix, decay: float) -> csr_matrix:
         """Weight the interaction matrix based on age of the events.
 
         :param X: Interaction matrix.
@@ -86,16 +96,10 @@ class TARSItemKNNLiu2012(TARSItemKNN):
         last_user_interactions = X.binary_values.multiply(timestamp_mat.max(axis=1))
         # the input for the decay is (t - t0_u) / tl_u
         # Where t0_u is the users first interaction and tl_u the last
-        timestamp_mat.data = self.decay_function(
+        timestamp_mat.data = LiuDecay(self.decay)(
             (timestamp_mat.data - first_user_interactions.data) / last_user_interactions.data
         )
         return csr_matrix(timestamp_mat)
-
-    def _add_decay_to_fit_matrix(self, X: InteractionMatrix) -> csr_matrix:
-        return self._add_decay_to_interaction_matrix(X)
-
-    def _add_decay_to_predict_matrix(self, X: InteractionMatrix) -> csr_matrix:
-        return self._add_decay_to_interaction_matrix(X)
 
     def _compute_users_first_interaction(self, X: InteractionMatrix) -> np.array:
         """Compute the launch time of each item as the first time it was interacted with.
