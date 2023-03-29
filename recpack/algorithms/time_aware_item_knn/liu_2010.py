106eaa75e6e6a27276541cacb435f679948c5b33
Lien Michiels <lienmichiels@Liens-MacBook-Pro-2.local>
2023-03-29 15:22:34 +0200

    Finish docstring for Liu

--- recpack/algorithms/time_aware_item_knn/liu_2010.py
+++ recpack/algorithms/time_aware_item_knn/liu_2010.py
@@ -11,44 +11,47 @@ from recpack.algorithms.time_aware_item_knn.base import TARSItemKNN
 
 
 class TARSItemKNNLiu(TARSItemKNN):
-    """ItemKNN algorithm where older interactions have less weight during both prediction and training.
+    """Time aware variant of ItemKNN which uses an exponential decay function and cosine similarity.
 
     Algorithm as described in
-    Nathan N. Liu, Min Zhao, Evan Xiang, and Qiang Yang. 
-    2010. Online evolutionary collaborative filtering. 
-    In Proceedings of the fourth ACM conference on Recommender systems (RecSys '10). 
-    Association for Computing Machinery, New York, NY, USA, 95–102. 
+    Nathan N. Liu, Min Zhao, Evan Xiang, and Qiang Yang.
+    2010. Online evolutionary collaborative filtering.
+    In Proceedings of the fourth ACM conference on Recommender systems (RecSys '10).
+    Association for Computing Machinery, New York, NY, USA, 95–102.
     https://doi.org/10.1145/1864708.1864729
 
-    Each interaction is decay to
+    The algorithm uses an exponential decay function:
 
     .. math::
 
-        e^{- \\alpha \\cdot \\text{age}}
+        \\Gamma(x) = e^{- \\alpha \\cdot \\text{x}}
 
-    Where alpha is the decay scaling parameter,
-    and age is the time between the maximal timestamp in the matrix
+    where alpha is the decay scaling parameter,
+    and x is the time between the maximal timestamp in the matrix
     and the timestamp of the event.
 
     Similarity is computed on this weighted matrix, using cosine similarity.
-
     At prediction time a user's history is weighted using the same formula with a different alpha.
     This weighted history is then multiplied with the precomputed similarity matrix.
 
     :param K: Amount of neighbours to keep. Defaults to 200.
-    :type K: int, Optional
+    :type K: int, optional
     :param fit_decay: Defines the decay scaling used for decay during model fitting.
-        Defaults to (1/3600).
-    :type fit_decay: float, Optional
+        Defaults to 1 / (24 * 3600).
+        This means for every day since an interaction, the value of it will be divided by 'e'.
+    :type fit_decay: float, optional
     :param predict_decay: Defines the decay scaling used for decay during prediction.
-        Defaults to (1/3600).
-    :type predict_decay: float, Optional
+        Defaults to 1 / (24 * 3600).
+        This means for every day since an interaction, the value of it will be divided by 'e'.
+    :type predict_decay: float, optional
     """
 
     def __init__(
         self,
         K: int = 200,
-        fit_decay: float = 1 / 3600,
-        predict_decay: float = 1 / 3600,
+        fit_decay: float = 1 / (24 * 3600),
+        predict_decay: float = 1 / (24 * 3600),
     ):
-        super().__init__(K=K, fit_decay=fit_decay, predict_decay=predict_decay, similarity="cosine")
+        super().__init__(
+            K=K, fit_decay=fit_decay, predict_decay=predict_decay, similarity="cosine", decay_function="exponential"
+        )
